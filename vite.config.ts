b03bdacc1ffba446f9d1d34cd41083bd8aabc324
withere <2673024411@qq.com>
2025-01-12 23:36:06 +0800

    add mock

--- vite.config.ts
+++ vite.config.ts
@@ -1,25 +1,34 @@
 import { fileURLToPath, URL } from 'node:url'
+import { UserConfigExport, ConfigEnv } from 'vite'
+import { viteMockServe } from 'vite-plugin-mock'
 
-import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
+
+
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [
-    vue(),
-    vueDevTools(),
-  ],
-  css: {
-    preprocessorOptions: {
-      scss: {
-        additionalData: '@use "@/styles/variables.scss" as *;'
+export default (({ command }: ConfigEnv): UserConfigExport => {
+  return {
+    plugins: [
+      vue(),
+      vueDevTools(),
+      viteMockServe({
+        mockPath: 'mock',
+        enable: command === 'serve',
+      }),
+    ],
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: '@use "@/styles/variables.scss" as *;'
+        }
       }
-    }
-  },
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
     },
-  },
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url))
+      },
+    },
+  }
 })
